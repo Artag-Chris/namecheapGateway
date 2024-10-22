467451bab3d893c0ac6aa42d5b9aa226cabb822d
Artag-Chris <scristxyz@gmail.com>
2024-10-22 16:41:30 -0500

    act

--- src/deceval/decevalgateway.routes.ts
+++ src/deceval/decevalgateway.routes.ts
@@ -2,22 +2,17 @@ import { Router } from "express";
 import { DecevalGatewayController } from "./decevalgateway.controller";
 
 export class DecevalGatewayRoutes {
+  static get routes() {
+    const router = Router();
 
-
-    static get routes(){ 
-    const router= Router();
-    
-    const decevalgatewayController =new DecevalGatewayController();
+    const decevalgatewayController = new DecevalGatewayController();
 
     //aqui iran las rutas de los servicios que usaremos de deceval
 
-    router.post(`/pagare`,decevalgatewayController.onLogin);
-  
-    router.post(`/consultservice`,decevalgatewayController.onConsult);
+    router.post(`/pagare`, decevalgatewayController.onLogin);
 
-    
+    router.post(`/consultservice`, decevalgatewayController.onConsult);
 
-return router;
- }
- 
+    return router;
+  }
 }
