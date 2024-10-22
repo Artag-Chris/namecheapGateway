cd024503d04a296fd4d6c61dacdc14145d8b8670
Artag-Chris <scristxyz@gmail.com>
2024-10-22 16:50:57 -0500

    pagare route

--- src/deceval/decevalgateway.service.ts
+++ src/deceval/decevalgateway.service.ts
@@ -1,24 +1,14 @@
-import { PrismaClient } from '@prisma/client';
 
 
-
-
-class DecevalGatewayService extends PrismaClient {
+class DecevalGatewayService  {
   constructor() {
-   // const dbHost = envs.DB_HOST;
-    //const dbPort = envs.DB_PORT;
-    //const dbUser = envs.DB_USER;
-    //const dbPassword = envs.DB_PASSWORD;
-    //const database = envs.DATABASE;
+   
 
-    super( );
-
-    this.init(); 
   }
   
    ///se cambiara los metodos 
   async onRequestLogin() {
-   
+   console.log("login");
 
     
   }
@@ -29,30 +19,5 @@ class DecevalGatewayService extends PrismaClient {
 
  
   
-  async init() {
-    try {
-      await this.$connect();
-      console.log(`Conexión a la base de datos establecida correctamente.`);
-    } catch (error) {
-      console.error('Error al conectar con la base de datos:', error);
-    }
-  }
-
-  async destroy() {
-    await this.$disconnect();
-  }
 }
-
-const decevalGatewayService = new DecevalGatewayService();
-
-process.on('SIGINT', async () => {
-  await decevalGatewayService.destroy();
-  process.exit(0);
-});
-
-process.on('SIGTERM', async () => {
-  await decevalGatewayService.destroy();
-  process.exit(0);
-});
-
 export default DecevalGatewayService;
