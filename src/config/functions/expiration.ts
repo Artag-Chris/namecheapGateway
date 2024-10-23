b881a177b69aa504b9f2a7bea04d6e31750d9bba
Artag-Chris <scristxyz@gmail.com>
2024-10-23 08:21:06 -0500

    act

--- src/config/functions/expiration.ts
+++
@@ -1,7 +0,0 @@
-export function sumar24Horas() {
-    const fechaActual = new Date();
-    const fechaSumada = new Date(fechaActual.getTime() + 24 * 60 * 60 * 1000);
-    return fechaSumada.toISOString()
-  }
-  
-
--- src/config/interfaces/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './auth';
-export * from './payment';
-export * from './aptplogin.res';
