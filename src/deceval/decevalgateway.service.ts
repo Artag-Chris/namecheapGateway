58225466232bf8d883da7028f7e28eff9a3aa2d6
Artag-Chris <scristxyz@gmail.com>
2024-10-24 08:57:19 -0500

    act

--- src/deceval/decevalgateway.service.ts
+++ src/deceval/decevalgateway.service.ts
@@ -1,36 +1,61 @@
 import XMLAdapter from "../config/adapters/js2xmlparser.adapter";
-import { ConsultGiradorDTO, ConsultPaymentDTO, CrearGiradorDTO, HeaderDTO } from "../domain";
+import { Request, Response } from "express";
+import {
+  ConsultGiradorDTO,
+  ConsultPaymentDTO,
+  CrearGiradorDTO,
+  CustomError,
+  HeaderDTO,
+} from "../domain";
 import { SolicitudCrearGiradorDTO } from "../domain/dtos/giradores/solicitudCrearGirador.DTO";
 
 export class DecevalGatewayService {
   constructor() {}
 
+
+
   ///se cambiara los metodos
-  async crearGirador(headerDTO: HeaderDTO,crearGiradorDTO: CrearGiradorDTO): Promise<string> {
-      
-     const [error, solicitudCrearGirador] = SolicitudCrearGiradorDTO.create({headerDTO,crearGiradorDTO});
-     if (error) return "error faltan campos";
-    const xml = XMLAdapter.jsonToXml("solicitudCrearGiradorServiceDTO", solicitudCrearGirador);
-    console.log(xml);// se debera mandar este objeto al proxy y esperar la respuesta
+  async crearGirador(
+    headerDTO: HeaderDTO,
+    crearGiradorDTO: CrearGiradorDTO
+  ): Promise<string> {
+  
+    const [error, solicitudCrearGirador] = SolicitudCrearGiradorDTO.create({
+      headerDTO,
+      crearGiradorDTO,
+    });
+    if (error) return `error faltan campos ${error}`;
+
+    const xml = XMLAdapter.jsonToXml(
+      "solicitudCrearGiradorServiceDTO",
+      solicitudCrearGirador
+    );
+    console.log(xml); // se debera mandar este objeto al proxy y esperar la respuesta
     return xml;
-    
   }
   async consultGirador(consultgiradorDTO: ConsultGiradorDTO): Promise<string> {
     const [error, consultGirador] = ConsultGiradorDTO.create(consultgiradorDTO);
-    if (error) return "error faltan campos";
-    
-    const xml = XMLAdapter.jsonToXml("consultaGiradorServiceDTO", consultGirador);
+    if (error) return `error faltan campos ${error}`;
+
+    const xml = XMLAdapter.jsonToXml(
+      "consultaGiradorServiceDTO",
+      consultGirador
+    );
     //el xml se envia al proxy
     console.log(xml);
     return xml;
-
   }
 
-  async retrieveCertificate(consultPaymentDTO:ConsultPaymentDTO ): Promise<string> {
+  async retrieveCertificate(
+    consultPaymentDTO: ConsultPaymentDTO
+  ): Promise<string> {
     const [error, consultPayment] = ConsultPaymentDTO.create(consultPaymentDTO);
     if (error) return "error faltan campos";
-    
-    const xml = XMLAdapter.jsonToXml("consultaPagareServiceDTO", consultPayment);
+
+    const xml = XMLAdapter.jsonToXml(
+      "consultaPagareServiceDTO",
+      consultPayment
+    );
     //el xml se envia al proxy
     console.log(xml);
     return xml;
@@ -38,8 +63,11 @@ export class DecevalGatewayService {
   async infoCertificate(payload: any) {
     const [error, consultPayment] = ConsultPaymentDTO.create(payload);
     if (error) return "error faltan campos";
-    
-    const xml = XMLAdapter.jsonToXml("consultaPagareServiceDTO", consultPayment);
+
+    const xml = XMLAdapter.jsonToXml(
+      "consultaPagareServiceDTO",
+      consultPayment
+    );
     //el xml se envia al proxy
     console.log(xml);
     return xml;
@@ -49,4 +77,3 @@ export class DecevalGatewayService {
     console.log(payload);
   }
 }
-
