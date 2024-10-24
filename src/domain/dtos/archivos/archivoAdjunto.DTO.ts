dca949d7fba9da163ccf0d763f7fa047f5e88759
Artag-Chris <scristxyz@gmail.com>
2024-10-24 10:36:51 -0500

    act

--- src/domain/dtos/archivos/archivoAdjunto.DTO.ts
+++ src/domain/dtos/archivos/archivoAdjunto.DTO.ts
@@ -3,4 +3,13 @@ export class ArchivoAdjuntoDTO {
        public readonly contenido: string | Buffer,
        public readonly nombreArchivo: string,
     ) {}
+    static create(object: {[key: string]: any}): [string?, ArchivoAdjuntoDTO?] {
+     const { contenido, nombreArchivo } = object;
+
+        if (!contenido || !nombreArchivo) {
+            return ["No se puede crear el archivo adjunto, faltan datos"];
+        }
+
+        return [undefined, new ArchivoAdjuntoDTO(contenido,nombreArchivo)];
+    }
 }
